d1500ca57683ce24f58a4164ddd1a680c303390f
unknown <letitslide87@gmail.com>
2018-07-09 15:27:19 -0400

    First commit!
    First full commit.

---
+++ Assets/depthMaskShader/scripts/button.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour {
+    public GameObject depthMashObject;
+    public bool meshIsMasking = true;
+
+	
+	public void OnButtonPress()
+    {
+        if (meshIsMasking)
+        {
+            depthMashObject.SetActive(false);
+            meshIsMasking = false;
+            return;
+        }
+        else
+        {
+            depthMashObject.SetActive(true);
+            meshIsMasking = true;
+        }
+
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
